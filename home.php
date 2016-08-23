8c2bd80bc3afb43f6cdf3437c356c6f223970143
William <xepity@gmail.com>
2016-08-23 14:37:24 +0100

    Core files updated

--- home.php
+++ home.php
@@ -1,5 +1,9 @@
 <?php 
 include ( "./inc/header.inc.php" );
-echo "Hello, ".$username;
-echo "<br /> Would you like to logout? <a href='logout.php'>Logout</a>";
 ?>
+
+<div id="subNavWrapper">
+    <ul class="subNav">
+        <li><?php echo "Hi there, ".$username; echo " Would you like to logout? <a href='logout.php'>Logout</a>";?></li>
+    </ul>
+</div>
--- inc/header.inc.php
+++ inc/header.inc.php
@@ -16,38 +16,15 @@ else
         <link rel="stylesheet" type="text/css" href="./css/style.css" />
         <script src="js/main.js" type="text/javascript"></script>
     </head>
-    <body background="./img/background.png">
-    	<div class="headerMenu">
-    		<div id="wrapper">
-    			<div class="logo">
-    				<img src="./img/ghost_squad_logo.png" />
-    			</div>
-    			<div class="search_box">
-    				<form action="search.php" method="GET" id="search">
-    					<input type="text" name="q">
-    				</form>
-    			</div>
-                <?php
-                if (!$username) {
-    			echo '<div id="menu">
-    				<a href="#" />Sign Up</a>
-    				<a href="#" />Sign In</a>
-    			</div>';
-            } else {
-                echo '<div id="menu">
-                    <a href="'.$username.'" />Profile</a>
-                    <a href="account_settings.php" />Account Settings</a>
-                    <!--<a href="#" style="background-color: #FF5858;"/>Admin</a>-->
-                    <a href="logout.php" />Logout</a>
-                </div>';
-            }
-            ?>
-    		</div>    	
-    	</div>
-        <div class="alertBar">
-            <marquee behavior="alternate" direction="left" width="40%"><b>Attention : This website is under development.</b></marquee>
-        </div>
-        <div id="wrapper">
-        <br />
+    <body>
+        <div id="navWrapper">
+    <ul class="navMain">
+        <li><a class="active" href="#">HOME</a></li>
+        <li><a href="#">MEMBERS</a></li>
+        <li><a href="#">STORE</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">FORUM</a></li>
+    </ul>
+</div>
     </body>
 </html>
